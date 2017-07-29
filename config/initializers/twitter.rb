3a7462f28612cf168da986dd5e973f87899e72c4
Justin Valentin <Justinvalentine88@gmail.com>
2017-07-29 15:50:52 -0400

    added database, created bot model

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "	VgmysLYNTNxK3EoMMmtFn18jV"
+  config.consumer_secret     = "MH23SGAhXbp0EcQdbJgJ9c08wrI3PxNg9vghGeMH3SYIDAzq2v"
+  config.access_token        = "891371921434173443-a3PezVodle64NQvlqQGnWnG4ZDqggWg"
+  config.access_token_secret = "bKjvB6gWWoPjsNkftXiD2LPMryZ0Fi8R9mqM1YJeBV4tO"
+end
