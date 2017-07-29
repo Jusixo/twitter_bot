5d2d90bde62a11e74b964e06328fcc8eee36a1f8
Justin Valentin <Justinvalentine88@gmail.com>
2017-07-29 16:54:11 -0400

    added response model

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,2 +1,5 @@
 class Bot < ApplicationRecord
+  client.search("to:The Iron Yard", result_type: "recent").take(3).each do |tweet|
+    puts tweet.text
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729194940) do
+ActiveRecord::Schema.define(version: 20170729205314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
