bcc23be9872d0573491719b29f25722247dd4ad3
Justin Valentin <Justinvalentine88@gmail.com>
2017-07-31 15:58:25 -0400

    setup the search and response for the twitter bot methods

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,5 +1,29 @@
-class Bot < ApplicationRecord
-  client.search("to:The Iron Yard", result_type: "recent").take(3).each do |tweet|
-    puts tweet.text
+class Bot < ActiveRecord::Base
+  def self.search_words words
+    CLIENT.search(words, lang: "en").first.text
+  end
+
+  # def self.loop words
+  #   CLIENT.search(words, lang: "en").take(5).each { |t|
+  #     tweets.push t.text
+  #   }
+  #   return tweets
+  # end
+
+  # code to find Iron Yard tweets and respond to them
+  def self.find_user number, words
+
+  # find tweets
+  CLIENT.search(words, lang: "en").take(number).each { |t|
+    # record twitter user and tweet num.
+    User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
+    # send flity message
+    CLIENT.update(Bot.respond(t.user.screen_name), in_reply_to_status_id: t.id)
+  }
+
+  end
+
+  def self.respond name
+    "@" + name + " " + Response.order_by_rand.first.message
   end
 end
