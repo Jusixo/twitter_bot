7f2503670687519026e57fd3d9ee6460ab3ae099
Justin Valentin <Justinvalentine88@gmail.com>
2017-07-31 13:44:27 -0400

    added users table to collect user information later to help make tweet bot smarter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729205314) do
+ActiveRecord::Schema.define(version: 20170731174140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170729205314) do
 
   create_table "responses", force: :cascade do |t|
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
